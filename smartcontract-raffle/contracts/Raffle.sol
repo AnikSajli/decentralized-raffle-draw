6343ae4034eb29710e66fc847da5ad5e759c7e8f
Abul Hasan Shajli <shajli.anik@vivacomsolutions.com>
2022-11-22 12:28:45 +0600

    raffle contract written

--- smartcontract-raffle/contracts/Raffle.sol
+++ smartcontract-raffle/contracts/Raffle.sol
@@ -2,15 +2,58 @@
 
 pragma solidity ^0.8.7;
 
-error Raffle_NotEnoughBalance();
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
 
-contract Raffle {
+/* Errors */
+error Raffle_NotEnoughBalance();
+error Raffle_TransferFailed();
+error Raffle_NotOpen();
+
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
+    
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+    
     /* State variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_participants;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
-    constructor(uint256 entranceFee) {
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerSelected(address indexed winner);
+
+    constructor (
+        uint256 entranceFee, 
+        address vrfCoordinatorV2, 
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit,
+        uint256 interval
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     function enterRaffle() public payable {
@@ -18,10 +61,53 @@ contract Raffle {
         if (msg.value < i_entranceFee) {
             revert Raffle_NotEnoughBalance();
         }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle_NotOpen();
+        }
+
         s_participants.push(payable(msg.sender));
     }
 
-    // function selectRandomWinner() {
+    function requestRandomWinner() external {
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
 
-    // }
+    function checkUpkeep(bytes calldata) external override {
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = 
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_participants.length;
+        address payable recentWinner = s_participants[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        s_participants = new address payable[](0);
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle_TransferFailed();
+        }
+        emit WinnerSelected(recentWinner);
+    }
+
+    function getRecentWinner() public view returns(address) {
+        return s_recentWinner;
+    }
+
+    function getEntranceFee() public view returns(uint256) {
+        return i_entranceFee;
+    }
+
+    function getParticipant(uint256 index) public view returns(address) {
+        return s_participants[index];
+    } 
 }
